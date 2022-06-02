357f634cdb81f018005276587d176a9c1b4ff31d
Guillermo Zafra <me@gzafra.com>
2022-06-02 21:24:30 +0200

    First version working

--- Sources/CRDTDictionary/CRDTDictionary.swift
+++ Sources/CRDTDictionary/CRDTDictionary.swift
@@ -1,6 +1,88 @@
-public struct CRDTDictionary {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+public class CRDTDictionary<ValueType: Hashable> {
+    var additions = [String: CRDTElement<ValueType>]()
+    var removals = [String: CRDTElement<ValueType>]()
+    
+    public init() {}
+    
+    func add(key: String, value: ValueType, timestamp: TimeInterval = Date.now.timeIntervalSince1970) {
+        let newValue = CRDTElement(value: value, timestamp: timestamp)
+        
+        guard let alreadyAdded = additions[key] else {
+            additions[key] = newValue
+            return
+        }
+        
+        if alreadyAdded.timestamp < timestamp {
+            additions[key] = newValue
+        }
+    }
+    
+    func remove(key: String, value: ValueType, timestamp: TimeInterval = Date.now.timeIntervalSince1970) {
+        let newValue = CRDTElement(value: value, timestamp: timestamp)
+        
+        guard let alreadyRemoved = removals[key] else {
+            removals[key] = newValue
+            return
+        }
+        
+        if alreadyRemoved.timestamp < timestamp {
+            removals[key] = newValue
+        }
+    }
+    
+    func allItems() -> [String: CRDTElement<ValueType>] {
+        additions.filter { key, addition in
+            guard let removed = removals[key] else { return true }
+            return removed.timestamp < addition.timestamp
+        }
+    }
+    
+    func update(key: String, value: ValueType, timestamp: TimeInterval = Date.now.gmt.timeIntervalSince1970) {
+        guard allItems()[key] != nil else { return }
+        add(key: key, value: value, timestamp: timestamp)
+    }
+    
+    
+    func merge(with other: CRDTDictionary<ValueType>) {
+        other.additions.forEach { key, element in
+            add(key: key, value: element.value, timestamp: element.timestamp)
+        }
+        other.removals.forEach { key, element in
+            remove(key: key, value: element.value, timestamp: element.timestamp)
+        }
+    }
+    
+    subscript(key: String) -> CRDTElement<ValueType>? {
+        allItems()[key]
+    }
+}
+
+class CRDTElement<T: Hashable>: Hashable {
+    let value: T
+    let timestamp: TimeInterval
+
+    internal init(value: T, timestamp: TimeInterval) {
+        self.value = value
+        self.timestamp = timestamp
+    }
+    
+    static func == (lhs: CRDTElement, rhs: CRDTElement) -> Bool {
+        lhs.value == rhs.value
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(value)
+    }
+}
+
+
+extension Date {
+    var gmt: Date {
+        let timeZone = TimeZone.current
+        let seconds: TimeInterval = Double(timeZone.secondsFromGMT(for:self))
+        let localDate = Date(timeInterval: seconds, since: self)
+        return localDate
     }
 }
--- Tests/CRDTDictionaryTests/CRDTDictionaryTests.swift
+++ Tests/CRDTDictionaryTests/CRDTDictionaryTests.swift
@@ -2,10 +2,123 @@ import XCTest
 @testable import CRDTDictionary
 
 final class CRDTDictionaryTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CRDTDictionary().text, "Hello, World!")
+    
+    // MARK: - Add & Remove
+    
+    func testAddItem() throws {
+        let crdtDictionary = CRDTDictionary<String>()
+        crdtDictionary.add(key: "key", value: "value")
+        XCTAssertEqual(crdtDictionary.additions.count, 1)
     }
+    
+    func testAddItemThenAddMoreRecentItem() throws {
+        let crdtDictionary = CRDTDictionary<String>()
+        crdtDictionary.add(key: "key", value: "value")
+        crdtDictionary.add(key: "key", value: "value2", timestamp: Date.distantFuture.timeIntervalSince1970)
+        XCTAssertEqual(crdtDictionary.additions.count, 1)
+        XCTAssertNotNil(crdtDictionary["key"])
+        XCTAssertEqual(crdtDictionary["key"]?.value, "value2")
+    }
+    
+    func testAddItemThenAddOlderItem() throws {
+        let crdtDictionary = CRDTDictionary<String>()
+        crdtDictionary.add(key: "key", value: "value")
+        crdtDictionary.add(key: "key", value: "value2", timestamp: Date.distantPast.timeIntervalSince1970)
+        XCTAssertEqual(crdtDictionary.additions.count, 1)
+        XCTAssertNotNil(crdtDictionary["key"])
+        XCTAssertEqual(crdtDictionary["key"]?.value, "value")
+    }
+    
+    func testRemoveItem() throws {
+        let crdtDictionary = CRDTDictionary<String>()
+        crdtDictionary.remove(key: "key", value: "value", timestamp: Date().timeIntervalSince1970)
+        XCTAssertEqual(crdtDictionary.removals.count, 1)
+    }
+    
+    func testRemoveItemThenRemoveMoreRecentItem() throws {
+        let crdtDictionary = CRDTDictionary<String>()
+        crdtDictionary.remove(key: "key", value: "value")
+        crdtDictionary.remove(key: "key", value: "value2", timestamp: Date.distantFuture.timeIntervalSince1970)
+        XCTAssertEqual(crdtDictionary.removals.count, 1)
+        XCTAssertNil(crdtDictionary["key"])
+    }
+    
+    func testAddItemThenRemoveMoreRecentItem() throws {
+        let crdtDictionary = CRDTDictionary<String>()
+        crdtDictionary.add(key: "key", value: "value")
+        crdtDictionary.remove(key: "key", value: "value2", timestamp: Date.distantFuture.timeIntervalSince1970)
+        XCTAssertEqual(crdtDictionary.additions.count, 1)
+        XCTAssertEqual(crdtDictionary.removals.count, 1)
+        XCTAssertNil(crdtDictionary["key"])
+    }
+    
+    func testAddItemThenRemoveOlderItem() throws {
+        let crdtDictionary = CRDTDictionary<String>()
+        crdtDictionary.add(key: "key", value: "value")
+        crdtDictionary.remove(key: "key", value: "value2", timestamp: Date.distantPast.timeIntervalSince1970)
+        XCTAssertEqual(crdtDictionary.additions.count, 1)
+        XCTAssertEqual(crdtDictionary.removals.count, 1)
+        XCTAssertNotNil(crdtDictionary["key"])
+        XCTAssertEqual(crdtDictionary["key"]?.value, "value")
+    }
+    
+    // MARK: - Update
+    
+    func testAddItemThenUpdateItem() throws {
+        let crdtDictionary = CRDTDictionary<String>()
+        crdtDictionary.add(key: "key", value: "value")
+        crdtDictionary.update(key: "key", value: "value2")
+        XCTAssertEqual(crdtDictionary.additions.count, 1)
+        XCTAssertEqual(crdtDictionary.removals.count, 0)
+        XCTAssertNotNil(crdtDictionary["key"])
+        XCTAssertEqual(crdtDictionary["key"]?.value, "value2")
+    }
+    
+    func testAddRemoveThenUpdateItem() throws {
+        let crdtDictionary = CRDTDictionary<String>()
+        crdtDictionary.add(key: "key", value: "value")
+        crdtDictionary.remove(key: "key", value: "value")
+        crdtDictionary.update(key: "key", value: "value2")
+        XCTAssertNil(crdtDictionary["key"])
+        XCTAssertNotEqual(crdtDictionary["key"]?.value, "value2")
+    }
+    
+    func testAddRemoveAddThenUpdate() throws {
+        let crdtDictionary = CRDTDictionary<String>()
+        crdtDictionary.add(key: "key", value: "value")
+        crdtDictionary.remove(key: "key", value: "value")
+        crdtDictionary.add(key: "key", value: "value")
+        crdtDictionary.update(key: "key", value: "value2")
+        XCTAssertNotNil(crdtDictionary["key"])
+        XCTAssertEqual(crdtDictionary["key"]?.value, "value2")
+    }
+    
+    // MARK: - Merge
+    
+    func testMergeTwoDictionariesOnlyAdditions() throws {
+        let crdtDictionary = CRDTDictionary<String>()
+        crdtDictionary.add(key: "key", value: "value", timestamp: Date.distantFuture.timeIntervalSince1970)
+        crdtDictionary.add(key: "key2", value: "value")
+        
+        let otherCrdtDictionary = CRDTDictionary<String>()
+        otherCrdtDictionary.add(key: "key", value: "value2")
+        otherCrdtDictionary.add(key: "key2", value: "value2")
+        
+        crdtDictionary.merge(with: otherCrdtDictionary)
+
+        XCTAssertEqual(crdtDictionary.additions.count, 2)
+        XCTAssertEqual(crdtDictionary["key"]?.value, "value")
+        XCTAssertEqual(crdtDictionary["key2"]?.value, "value2")
+    }
+    
+    func testMergeTwoDictionariesCommutative() throws {
+        let crdtDictionary = CRDTDictionary<String>()
+        crdtDictionary.add(key: "key", value: "value")
+        crdtDictionary.remove(key: "key", value: "value")
+        crdtDictionary.add(key: "key", value: "value")
+        crdtDictionary.update(key: "key", value: "value2")
+        XCTAssertNotNil(crdtDictionary["key"])
+        XCTAssertEqual(crdtDictionary["key"]?.value, "value2")
+    }
+
 }
